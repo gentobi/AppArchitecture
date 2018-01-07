88956c57f0d9d72f2ec358af7bf506d65c684a42
tannguyen <nhattannguyen1610@gmail.com>
2018-01-07 23:41:36 +0700

    DevExtreme integrated

--- RS.AngularUI/src/app/app.component.ts
+++ RS.AngularUI/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+	minDate = new Date(2000, 0, 1);
+	maxDate = new Date(2029, 11, 31);
+	currentDate = new Date();
 }
